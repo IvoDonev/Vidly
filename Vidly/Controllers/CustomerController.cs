fd3a01bd85659c8dcc9ead8c5b7ce42cb0061bfd
Ivaylo Donev <id143230@renishaw.com>
2019-06-12 09:44:39 +0100

    Completed Module 2

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CustomersViewModels _viewModel = new CustomersViewModels()
+        {
+            Customers = new List<Models.Customer>()
+            {
+                new Models.Customer() { Name = "Adam", Id = 1},
+                new Models.Customer() { Name = "Batman", Id = 2 }
+            }
+        };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _viewModel.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer != null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MoviesViewModel viewModel = new MoviesViewModel()
+        {
+            Movies = new List<Models.Movie>()
+            {
+                new Models.Movie() { Name = "Shrek", Id = 1 },
+                new Models.Movie() { Name = "Cinderella", Id = 2}
+            }
+        };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(viewModel);
+        }
+    }
+}
